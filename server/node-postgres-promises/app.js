b55b380ce182b669e934f43d727d7a72c06a72d2
Andy Chen <chen1370@purdue.edu>
2017-02-01 04:09:48 +0000

    Added origin header

--- server/node-postgres-promises/app.js
+++ server/node-postgres-promises/app.js
@@ -21,6 +21,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://openalexandria.us.to");
+      res.header("Access-Control-Allow-Credentials", "true");
+        next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
