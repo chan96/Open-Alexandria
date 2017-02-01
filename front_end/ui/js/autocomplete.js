ba84bbb6563f25cdce85ca2acbe593529c32c696
Brian Chan <chan96@purdue.edu>
2017-01-31 22:58:08 -0500

    fixed autocomplete

--- front_end/ui/js/autocomplete.js
+++ front_end/ui/js/autocomplete.js
@@ -1,29 +1,7 @@
 
-$( "#autocomplete" ).autocomplete({
-    source: function( request, response ) {
-        $.ajax({
-            dataType: "json",
-            type : 'Get',
-            url: 'Openalexandria.us.to:4074/getRelatedItems/open',
-            success: function(data) {
-                $('#autocomplete').removeClass('ui-autocomplete-loading');  
-                // hide loading image
-                alert("hi");
-                response( $.map( data, function(item) {
-                    // your operation on data
-                                    alert("hi");
-
-                }));
-            },
-            error: function(data) {
-                alert("hi");
-                $('#autocomplete').removeClass('ui-autocomplete-loading');  
-            }
-        });
-    },
-    minLength: 3,
-    open: function() {},
-    close: function() {},
-    focus: function(event,ui) {},
-    select: function(event, ui) {}
+$('#autocomplete').autocomplete({
+    serviceUrl: 'http://Openalexandria.us.to:4074/getRelatedItems/',
+    onSelect: function (suggestion) {
+        alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+    }
 });
