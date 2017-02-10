8028a5abab7ef11b01ad4722babef8330e81025d
chan96 <chan96@purdue.edu>
2017-02-10 15:01:33 -0500

    add create question and answer functions

---
+++ front_end/ui/js/qa.js
@@ -0,0 +1,54 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+function getQuestions(courseID) {
+    
+}
+
+function getAnswers(questionID) {
+    
+}
+
+function createQuestion(cn, cd, s) {
+    $.ajax({
+        type: "POST",
+        url: 'http://Openalexandria.us.to/createQuestion/',
+        data: ({
+                courseid    : cId,
+                userid      : uId,
+                title       : t,
+                body        : b
+                }),
+        dataType: "html",
+        success: function(data) {
+            console.log(data.message);
+        },
+        error: function(xhr, status, error) {
+            var err = eval("(" + xhr.responseText + ")");
+            alert(err.Message);
+        }
+    });
+}
+
+
+function createAnswer(cn, cd, s) {
+    $.ajax({
+        type: "POST",
+        url: 'http://Openalexandria.us.to/createAnswer/',
+        data: ({
+                questionid  : qnId,
+                courseid    : cId,
+                userid      : uId,
+                body        : b,
+                }),
+        dataType: "html",
+        success: function(data) {
+            console.log(data.message);
+        },
+        error: function(xhr, status, error) {
+            var err = eval("(" + xhr.responseText + ")");
+            alert(err.Message);
+        }
+    });
+}
