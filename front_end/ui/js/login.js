e691fe163fcad1881d2a2b77cf48d5fe18909aac
Xu Han <han308@purdue.edu>
2017-02-14 18:09:14 -0500

    Login.js change

--- front_end/ui/js/login.js
+++ front_end/ui/js/login.js
@@ -4,11 +4,12 @@ function submitLogin(form){
   var formData = $(form).serializeArray();
   console.log("FUCK");
   $.post(theUrl, formData, function (data) {
-        console.log("SHIT");
+        //console.log("SHIT");
         window.location.href = "http://localhost:3000";
         //window.location.href = 'http://openalexandria.us.to:3000/login.html';
     }).done(function(){
       console.log("DOONE");
+      //document.cookie;
 
     }).fail(function (){
         $("#incorrect").html("<p>Login Incorrect</p>");
