ff63489255e3f6388d15ff9220fbbbfe44a0d6c3
Brian Chan <chan96@purdue.edu>
2017-02-15 02:43:38 +0000

    newcourse.js fix for incorrect route

--- front_end/ui/js/newcourse.js
+++ front_end/ui/js/newcourse.js
@@ -28,7 +28,7 @@ function validate() {
 function createCourse(cn, cd, s) {
      $.ajax({
         type: "POST",
-        url: globalUrl + 'getCourseKeyword/',
+        url: globalUrl + 'addNewCourse/',
         data: ({ coursename : cn, coursedescription: cd, school : s}),
         dataType: "html",
         success: function(data) {
