b6dac4307638e1185202e49954b81616a37d9ed8
Xu Han <han308@purdue.edu>
2017-02-18 17:46:32 -0500

    admin change

--- front_end/ui/js/admin.js
+++ front_end/ui/js/admin.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 
   var courseUrl = globalUrl + "getAllCourse";
   var userUrl = globalUrl +"listAllUsers";
-  questionUrl = globalUrl + "getAllQuestions?uniqueid=";
+  questionUrl = globalUrl + "getAllQuestions?courseid=";
 
   	//COURSE
   	$.get(courseUrl, function (data) {
