628205151291d6b23daa8dd915324df61efc560b
Brian Chan <chan96@purdue.edu>
2017-02-18 15:08:06 -0500

    use http in globalurl

--- front_end/ui/js/global.js
+++ front_end/ui/js/global.js
@@ -1,5 +1,5 @@
 //var globalUrl = 'http://ec2-52-24-159-251.us-west-2.compute.amazonaws.com:3000/';
 //var globalUrl = 'http://openalexandria.us.to/';
-var globalUrl = 'https://openalex.com/';
+var globalUrl = 'http://openalex.com/';
 //var globalUrl = 'http://openalexandria.us.to:3003/';
 //var globalUrl = 'file:///C:/Users/chan96/Open-Alexandria/front_end/ui/';
