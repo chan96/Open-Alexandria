79d2f5d70bb4fa8ac10d454b13ec15ced96cf0cf
Brian Chan <chan96@purdue.edu>
2017-03-24 00:07:47 +0000

    document loader uses google viewer now

--- front_end/ui/js/document_view.js
+++ front_end/ui/js/document_view.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   var url = decodeURIComponent(getUrlParameter('docUrl'));
-  console.log('li ' + url);
-  $('#doc-frame').attr('src', url);
+  console.log('https://view.officeapps.live.com/op/embed.aspx?src=' + url);
+  //use microsoft viewer 
+  //$('#doc-frame').attr('src', 'https://view.officeapps.live.com/op/embed.aspx?src=' + url);
+  
+  //use google doc viewer
+  $('#doc-frame').attr('src', 'http://docs.google.com/gview?url=' + url + '&embedded=true');
   $('#doc-name').text(getUrlParameter('docName'));
 })
