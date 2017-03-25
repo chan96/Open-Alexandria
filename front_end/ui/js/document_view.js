6d64a45766d0b764e5a7e249e14391f2631457e8
Brian Chan <chan96@purdue.edu>
2017-03-25 18:35:30 +0000

    use https google

--- front_end/ui/js/document_view.js
+++ front_end/ui/js/document_view.js
@@ -5,6 +5,6 @@ $(document).ready(function() {
   //$('#doc-frame').attr('src', 'https://view.officeapps.live.com/op/embed.aspx?src=' + url);
   
   //use google doc viewer
-  $('#doc-frame').attr('src', 'http://docs.google.com/gview?url=' + url + '&embedded=true');
+  $('#doc-frame').attr('src', 'https://docs.google.com/gview?url=' + url + '&embedded=true');
   $('#doc-name').text(getUrlParameter('docName'));
 })
