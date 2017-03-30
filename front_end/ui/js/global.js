a884717fba2789ec710be09ca631f59abc1f1d4e
Xu Han <han308@purdue.edu>
2017-03-30 13:29:43 -0400

    Disabled documents can be seen by the uploaded user to renable it

--- front_end/ui/js/global.js
+++ front_end/ui/js/global.js
@@ -1,9 +1,9 @@
 //var globalUrl = 'http://ec2-52-24-159-251.us-west-2.compute.amazonaws.com:3001/';
 //var globalUrl = 'http://openalexandria.us.to/';
-var globalUrl = 'https://openalex.com/';
+//var globalUrl = 'https://openalex.com/';
 //var globalUrl = 'http://openalexandria.us.to:3003/';
 //var globalUrl = 'http://localhost:3001/'
-//var globalUrl = 'http://localhost:3001/'
+var globalUrl = 'http://localhost:3001/'
 //var globalUrl = 'file:///C:/Users/chan96/Open-Alexandria/front_end/ui/';
 
 var getUrlParameter = function getUrlParameter(sParam) {
