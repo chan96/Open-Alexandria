70a829d245b3b0dec6555e1ea43b261da63c9132
Brian Chan <chan96@purdue.edu>
2017-03-30 03:41:33 +0000

    test upload

--- front_end/ui/js/newDocument.js
+++ front_end/ui/js/newDocument.js
@@ -16,14 +16,12 @@ $(document).ready(function() {
       var desc;
 
       // loop through all the selected files and add them to the formData object
-      for (var i = 0; i < files.length; i++) {
-        var file = files[i];
+        var file = files[0];
 
         // add the files to formData object for the data payload
-        formData.append('files[]', file, file.name);
+        formData.append('document', file, file.name);
         type = 'pdf';
         desc = 'test';
-      }
 
       $.ajax({
         url: globalUrl + 'uploadDocuments?courseid=' + courseid + '&type=' + type + '&description=' + desc,
@@ -34,33 +32,8 @@ $(document).ready(function() {
         success: function(data){
           console.log('upload successful!\n' + data);
         },
-        xhr: function() {
-          // create an XMLHttpRequest
-          var xhr = new XMLHttpRequest();
-
-          // listen to the 'progress' event
-          xhr.upload.addEventListener('progress', function(evt) {
-
-            if (evt.lengthComputable) {
-              // calculate the percentage of upload completed
-              var percentComplete = evt.loaded / evt.total;
-              percentComplete = parseInt(percentComplete * 100);
-
-              // update the Bootstrap progress bar with the new percentage
-              //$('.progress-bar').text(percentComplete + '%');
-              //$('.progress-bar').width(percentComplete + '%');
-
-              // once the upload reaches 100%, set the progress bar text to done
-              if (percentComplete === 100) {
-                //$('.progress-bar').html('Done');
-                console.log('done');
-              }
-
-            }
-
-          }, false);
-
-          return xhr;
+        error: function(xhr, status, error) {
+          console.log(xhr.responseText);
         }
       });
 
