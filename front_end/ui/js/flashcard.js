f4fe89a0e73f37c9c592cc4b16090f3fe88c9ea5
chan96 <chan96@purdue.edu>
2017-04-07 16:52:50 -0400

    forgot to add flashcard.js

---
+++ front_end/ui/js/flashcard.js
@@ -0,0 +1,58 @@
+function setNewFlashcardListener() {
+     $('#flashcardModal').on('show.bs.modal', function (e) {
+        if (document.cookie == '') {
+          
+          location.href = globalUrl + '/login.html' + '?redirect=' + location.href;
+        }
+    })
+    $('#postFlashcardBttn').click(function () {
+        var flashcardTitle = $('#flashcard-title').val();
+        var courseID = getUrlParameter('courseid');
+        var flashcardBttn = $('#postFlashcardBttn');
+        console.log(flashcardTitle + ' ' + courseID);
+
+        postBttn.prop('disabled', true);
+        postBttn.text('Submitting...');
+
+        $.ajax({
+            type: 'POST',
+            url: globalUrl + 'createDeck/' + '?courseid=' + courseID + '&deckname=' + flashcardTitle,
+            //contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+            success: function(data) {
+                console.log(data);
+                postBttn.prop('disabled', false);
+                postBttn.text('Create deck!');
+
+                location.reload();
+            },
+            error: function(xhr, status, error) {
+                var err = eval("(" + xhr.responseText + ")");
+                console.log(err.Message);
+                postBttn.prop('disabled', false);
+                postBttn.text('Create deck!');
+                //alert(data.error.message);
+
+
+            }
+        });
+    });
+}
+
+function getFlashcardDecks(courseID) {
+     $.ajax({
+        type: "GET",
+        url: globalUrl + 'searchFlashDeckNameByCourse/',
+        data: ({ courseid : courseID}),
+        dataType: "json",
+        success: function(data) {
+            var 
+
+            console.log(data);
+             
+        },
+        error: function(data) {
+            console.log(data.error.message);
+
+        }
+    });
+}
