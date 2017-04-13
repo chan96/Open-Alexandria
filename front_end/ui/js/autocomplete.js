6ac4e6b15e22afa7f55415c64347d60e8296895a
Xu Han <han308@purdue.edu>
2017-04-13 19:39:52 -0400

    Working on search by document

--- front_end/ui/js/autocomplete.js
+++ front_end/ui/js/autocomplete.js
@@ -1,22 +1,69 @@
+$("input[id='classes']").change(function(){
+                console.log("Classes checked");
+            $('#autocomplete').autocomplete({
 
-$('#autocomplete').autocomplete({
+                serviceUrl: globalUrl + 'getCourseKeyword/',
+                onSearchComplete: function (query, suggestions) {
+                    console.log(suggestions);
+                },
+                onSelect: function (suggestion) {
+                //alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+                console.log(suggestion.data);
+                console.log(suggestion.data.coursename);
 
-    serviceUrl: globalUrl + 'getCourseKeyword/',
-    onSearchComplete: function (query, suggestions) {
-        console.log(suggestions);
-    },
-    onSelect: function (suggestion) {
-        //alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
-        console.log(suggestion.data);
-        console.log(suggestion.data.coursename);
+                location.href = globalUrl + 'search.html?courseid=' +  suggestion.data.courseuniqueid + '&coursename=' + suggestion.data.coursename;
 
-        location.href = globalUrl + 'search.html?courseid=' +  suggestion.data.courseuniqueid + '&coursename=' + suggestion.data.coursename;
+            },
 
-    },
-    
-    showNoSuggestionNotice: true
-})
+            showNoSuggestionNotice: true
 
+
+        });
+});
+$("input[id='flashcards']").change(function(){
+            console.log("flashcards checked");
+            $('#autocomplete').autocomplete({
+
+                serviceUrl: globalUrl + 'getDocumentsFromTag/',
+                onSearchComplete: function (query, suggestions) {
+                    console.log(suggestions);
+                },
+                onSelect: function (suggestion) {
+                //alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+                console.log(suggestion.data);
+                console.log(suggestion.data.coursename);
+
+                location.href = globalUrl + 'search.html?courseid=' +  suggestion.data.courseuniqueid + '&coursename=' + suggestion.data.coursename;
+
+            },
+
+            showNoSuggestionNotice: true
+
+
+        });
+});
+$("input[id='files']").change(function(){
+            console.log("files checked");
+            $('#autocomplete').autocomplete({
+
+                serviceUrl: globalUrl + 'getDocumentsFromTag/',
+                onSearchComplete: function (query, suggestions) {
+                    console.log(suggestions);
+                },
+                onSelect: function (suggestion) {
+                //alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+                console.log(suggestion.data);
+                console.log(suggestion.data.coursename);
+
+                location.href = globalUrl + 'search.html?courseid=' +  suggestion.data.courseuniqueid + '&coursename=' + suggestion.data.coursename;
+
+            },
+
+            showNoSuggestionNotice: true
+
+
+        });
+});
 /*$( "#txtcity" ).autocomplete({
   // do whatever
 }).val('Banana').data('autocomplete')._trigger('select');*/
