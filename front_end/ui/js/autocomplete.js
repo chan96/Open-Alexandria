d7f09a7090641d922343bb7ef3734cc75f10b542
Xu Han <han308@purdue.edu>
2017-04-22 14:56:38 -0400

    Onchange

--- front_end/ui/js/autocomplete.js
+++ front_end/ui/js/autocomplete.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
     $("input[id='classes']").prop("checked", true);
-    $("input[id='classes']").trigger('change');
+    if($("input[id='classes']").is(':checked')){
+                    $('#autocomplete').autocomplete({
+
+                serviceUrl: globalUrl + 'getCourseKeyword/',
+                onSearchComplete: function (query, suggestions) {
+                    console.log(suggestions);
+                },
+                onSelect: function (suggestion) {
+                //alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+                console.log(suggestion.data);
+                console.log(suggestion.data.coursename);
+
+                location.href = globalUrl + 'search.html?courseid=' +  suggestion.data.courseuniqueid + '&coursename=' + suggestion.data.coursename;
+
+            },
+
+            showNoSuggestionNotice: true
+
+
+        });
+    }
 }
 
 $("input[id='classes']").click(function(){
